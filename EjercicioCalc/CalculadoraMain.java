33b5b741546c5514554d85eec542129626b86f8a
Gari <garikworkaddress@gmail.com>
2025-03-19 10:02:46 +0100

    Creacion de metodo de selección de números. Primeros pasos del menú. Primeros pasos de la selección de operación.

--- EjercicioCalc/CalculadoraMain.java
+++ EjercicioCalc/CalculadoraMain.java
@@ -1,37 +1,54 @@
 package EjercicioCalc;
+import java.util.Scanner;
+
 import EjercicioCalc.Calculadora;
 
 public class CalculadoraMain {
     public static void main(String[] args) {
-        int opcion = sc.nextInt();
 
-        if (opcion == 1) {
+        Scanner sc = new Scanner(System.in);
+        Calculadora calcu = new Calculadora();
+        boolean programa = true;
 
-            System.out.println("Resultado: " + (numero1 + numero2));
-
-        } else if (opcion == 2) {
-
-            System.out.println("Resultado: " + (numero1 - numero2));
-
-        } else if (opcion == 3) {
-
-            System.out.println("Resultado: " + (numero1 * numero2));
-
-        } else if (opcion == 4) {
-
-            if (numero2 == 0) {
-
-                System.out.println("Error: No se puede dividir por cero.");
-
-            } else {
-
-                System.out.println("Resultado: " + (numero1 / numero2));
+        System.out.println("Bienvenido al programa de calculadora.");
+        do {
+            System.out.println("Seleccione lo que desea hacer: \n1: Establecer números\n2: Calcular\n3: Salir");
+            int eleccionMenuUno = sc.nextInt();
+            switch (eleccionMenuUno){
+                case 1:
+                ponerNumeros(sc, calcu);
+                break;
+                case 2:
+                if(calcu.numero1Getter()==0 && calcu.numero2Getter()==0){
+                    System.out.println("Ambos números son 0. No es posible continuar con la operación.");
+                    break;
+                } else {
+                calculos(sc, calcu);
+                }
+                break;
 
             }
-        } else {
+        } while (programa);
 
-            System.out.println("Opción no válida.");
+        
 
-        }
+        sc.close();
+    }
+
+    public static void ponerNumeros(Scanner sc, Calculadora calcu){
+        System.out.println("Introduzca el primer número:");
+        double tempNumero1 = sc.nextDouble();
+        calcu.numero1Setter(tempNumero1);
+        System.out.println("Introduzca el segundo número:");
+        tempNumero1 = sc.nextDouble();
+        calcu.numero2Setter(tempNumero1);
+        System.out.println("Operación ejecutada con éxito.");
+        return;
+    }
+
+    public static void calculos(Scanner sc, Calculadora calcu){
+        System.out.println("Seleccione su operación. \n1: Suma\n2: Resta\n3: Multiplicación\n4: División\n5: Potencia\n6: Raíz");
+        
+
     }
 }
